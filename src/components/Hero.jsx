d92b7a798baacf476a6a8bc512fffb74957cbbae
Irfankhan20 <sipathan96@gmail.com>
2025-03-07 13:46:13 +0600

    some change on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,16 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    // this is hero
-    <section className={`relative w-full h-screen mx-auto`}>
+    <section className="relative w-full h-screen max-h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[90px] md:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 h-40 sm:h-80 violet-gradient" />
         </div>
 
         <div>
@@ -20,21 +18,23 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Irfan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a MERN Stack Web Developer. <br className="sm:block hidden" />
-            Thanks for Visit my Portfolio
+            I am a MERN Stack Web Developer.
+            <br className="sm:block hidden" />
+            Thanks for visiting my Portfolio.
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* Ensure ComputersCanvas does not break mobile layout */}
+      <div className="w-full h-full flex justify-center items-center">
+        <ComputersCanvas />
+      </div>
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
+              animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
