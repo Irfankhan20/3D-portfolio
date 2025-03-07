34a4658a7afc84ab46f626df600f30478f0afa1b
Irfankhan20 <sipathan96@gmail.com>
2025-03-07 14:10:28 +0600

    hero section change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,50 +1,27 @@
-import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen max-h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto">
       <div
-        className={`absolute inset-0 top-[90px] md:top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        data-aos="fade-right"
+        data-aos-duration="2000"
+        className={`${styles.paddingX} absolute inset-0 top-[70px] md:top-[90px] lg:top-[110px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-          <div className="w-1 h-40 sm:h-80 violet-gradient" />
-        </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Irfan</span>
+            Hi, I'm <span className="text-[#915]">Rime</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a MERN Stack Web Developer.
-            <br className="sm:block hidden" />
-            Thanks for visiting my Portfolio.
+            I'm a developer with expertise in full-stack development. <br /> I
+            create seamless, user-centric solutions that blend creativity and
+            functionality.
           </p>
         </div>
       </div>
 
-      {/* Ensure ComputersCanvas does not break mobile layout */}
-      <div className="w-full h-full flex justify-center items-center">
-        <ComputersCanvas />
-      </div>
-
-      <div className="absolute bottom-10 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{ y: [0, 24, 0] }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
-      </div>
+      <ComputersCanvas />
     </section>
   );
 };
